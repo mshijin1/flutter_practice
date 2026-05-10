5f6560e33f798c6a445c1d4d0b18939f9004f7e0
m.shijin <mshijin251@gmail.com>
2026-05-10 19:38:39 +0530

    elevated button stateless widget practice

--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,29 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget{
   const MyApp({super.key});
+
+  void know() {
+    // ignore: avoid_print
+    print("Button clicked");
+  }
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-
-      // scaffold: widget
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:
-        Scaffold(
-          body: Center(child: Text('Body Text')),
-          appBar: AppBar(title: Text('App Bar Title'), backgroundColor: Colors.purple, foregroundColor: Colors.white,),
-        )
+      home: Scaffold(
+        appBar: AppBar(title: const Text("My App"), backgroundColor: const Color.fromARGB(255, 229, 247, 94)),
+        body: Center(child: ElevatedButton(
+          style: const ButtonStyle(
+            backgroundColor: WidgetStatePropertyAll<Color>(Colors.green),
+            ),
+            child: const Text("Click Me fast"),
+            onPressed: () {know();},
+            ),
+          ),
+      ),
     );
   }
 }
